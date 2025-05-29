a6e322aaba866358b08481f11c20f1e00552b1bd
NadetDev <nadetdev@gmail.com>
2025-05-29 18:35:49 +0200

    add deps + clean project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import App from './App.vue'
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import router from './router/routes'
 
 const app = createApp(App)
+const pinia = createPinia()
+
+app.use(router)
+app.use(pinia)
 
 app.mount('#app')

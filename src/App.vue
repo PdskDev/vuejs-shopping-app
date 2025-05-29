d09a8c768fcb4a47aceb817398e250e780caaa87
NadetDev <nadetdev@gmail.com>
2025-05-29 20:09:26 +0200

    add bootstrap + refacto header + footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
-  <RouterView />
+  <div class="d-flex flex-column min-vh-100">
+    <HeaderTpl />
+    <div class="flex-grow-1">
+      <RouterView />
+    </div>
+    <FooterTpl />
+  </div>
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router'
+import HeaderTpl from './components/layout/HeaderTpl.vue'
+import FooterTpl from './components/layout/FooterTpl.vue'
 </script>
---
+++ src/components/layout/FooterTpl.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer class="border-top footer text-muted p-2 bg-body-tertiary">
+    <div class="text-center">NadetDev <i class="bi bi-code-slash"></i> Vue.js Shopping App</div>
+  </footer>
+</template>

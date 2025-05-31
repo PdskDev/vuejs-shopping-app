837f3ceda82ba5677b707b1aa310fbe3070c8ef6
NadetDev <nadetdev@gmail.com>
2025-05-31 04:48:31 +0200

    close Model + redirect to Contact Us

--- src/components/product/ProductDetail.vue
+++ src/components/product/ProductDetail.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="modal fade" tabindex="-1" :id="`product-detail-modal-${product.id}`">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-dialog-centered modal-xl">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title">Modal title</h5>
+          <h5 class="modal-title text-muted fs-5">Product Details</h5>
           <button
             type="button"
             class="btn-close"
@@ -12,21 +12,91 @@
           ></button>
         </div>
         <div class="modal-body">
-          <p>Modal body text goes here.</p>
+          <div class="row g-4">
+            <div class="col-md-6">
+              <div class="">
+                <img
+                  :src="product.image || `https://placehold.co/600x400`"
+                  :alt="product.name"
+                  class="img-fluid rounded-3"
+                  style="max-height: 400px"
+                />
+              </div>
+            </div>
+
+            <div class="col-md-6">
+              <h2 class="text-success">{{ product.name }}</h2>
+              <span class="badge text-bg-warning p-2" v-if="product.isBestseller">
+                <i class="bi bi-star-fill"></i> Bestseller
+              </span>
+              <span class="badge text-bg-warning p-2 mx-2">
+                <i class="bi bi-tag-fill px-1"></i>
+                <span> {{ product.category }}</span>
+              </span>
+              <p class="text-muted">{{ product.description }}</p>
+
+              <div class="h3">
+                <span
+                  class="text-success"
+                  v-if="!product.salePrice"
+                  :style="{ textDecoration: product.salePrice ? 'line-through' : 'none' }"
+                >
+                  ${{ product.price.toFixed(0) }}
+                </span>
+                <!--Add text-dcoration line through if product.saleprice is present-->
+                <span class="text-danger" v-if="product.salePrice">
+                  ${{ product.salePrice.toFixed(0) }} SALE!
+                </span>
+              </div>
+
+              <div class="">
+                <div class="d-flex align-items-center gap-2 mb-2"></div>
+                <div class="d-flex flex-wrap gap-2">
+                  <span
+                    class="badge text-bg-secondary p-2"
+                    v-for="(tag, index) in product.tags"
+                    :key="index"
+                  >
+                    {{ tag }}</span
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
+          <button type="button" class="btn btn-success" @click="handleContactUS(product.id)">
+            contact Us
+          </button>
+          <button
+            :id="`btn-close-modal-${product.id}`"
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            Close
+          </button>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script setup>
+import { useRouter } from 'vue-router'
+import { APP_ROUTE_NAMES } from '@/constants/routeNames'
+
 const props = defineProps({
   product: {
     type: Object,
     required: true,
   },
 })
+
+const router = useRouter()
+const handleContactUS = (productId) => {
+  document.getElementById(`btn-close-modal-${productId}`).click()
+  router.push({
+    name: APP_ROUTE_NAMES.CONTACT_US,
+  })
+}
 </script>

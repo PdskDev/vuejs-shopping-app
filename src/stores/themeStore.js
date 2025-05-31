6b13937c40412d801b2ae6657be111116171a742
NadetDev <nadetdev@gmail.com>
2025-05-31 23:03:19 +0200

    refacto theme store

--- src/stores/themeStore.js
+++ src/stores/themeStore.js
@@ -1,14 +1,17 @@
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
-export const useThemeStore = defineStore('themeStore', {
-  state: () => ({
-    theme: 'light',
-  }),
-  actions: {
-    setTheme(newTheme) {
-      this.theme = newTheme
-      const bodyElement = document.querySelector('body')
-      bodyElement.setAttribute('data-bs-theme', this.theme)
-    },
-  },
+export const useThemeStore = defineStore('themeStore', () => {
+  const theme = ref('light')
+
+  const setTheme = (newTheme) => {
+    theme.value = newTheme
+    const bodyElement = document.querySelector('body')
+    bodyElement.setAttribute('data-bs-theme', theme.value)
+  }
+
+  return {
+    theme,
+    setTheme,
+  }
 })
